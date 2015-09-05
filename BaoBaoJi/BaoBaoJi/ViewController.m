8e8d85c972e9fc17d9ac3c25f1c5ae0bfe751902
iosfeng <iosfeng@163.com>
2015-09-05 11:39:12 +0800

    初始化项目

---
+++ BaoBaoJi/BaoBaoJi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaoBaoJi
+//
+//  Created by efeng on 15/9/5.
+//  Copyright (c) 2015年 buerguo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
